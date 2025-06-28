374ca9524acc173fe75cfa27ae7e470049e4f856
Adrian <Josephmateomb@hotmail.com>
2025-06-28 01:15:08 -0500

    medicion de baja o funcionamiento correcto del panel solar

--- datos_arduino/views.py
+++ datos_arduino/views.py
@@ -4,3 +4,8 @@ from .models import SensorData
 def lista_datos(request):
     datos = SensorData.objects.order_by('-timestamp')[:10]  # Últimos 30
     return render(request, 'datos_arduino/lista_datos.html', {'datos': datos})
+
+def lista_datos(request):
+    datos = SensorData.objects.order_by('-timestamp')[:10]
+    ultimo = datos[0] if datos else None
+    return render(request, 'datos_arduino/lista_datos.html', {'datos': datos, 'ultimo': ultimo})
